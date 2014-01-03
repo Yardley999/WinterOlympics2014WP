da477e4fb56e3e9406a7b7684be97c59291eaeb0
elecpiano <elecpiano@gmail.com>
2014-01-03 23:23:57 +0800

    initial project

---
+++ WinterOlympics2014WP/WinterOlympics2014WP/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WinterOlympics2014WP.Resources;
+
+namespace WinterOlympics2014WP
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
