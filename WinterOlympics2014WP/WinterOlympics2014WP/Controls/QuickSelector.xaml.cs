38e3d45040cc8787b3a45ee4f037e61e93e94aef
elecpiano <elecpiano@gmail.com>
2014-01-04 03:14:02 +0800

    quick selector

---
+++ WinterOlympics2014WP/WinterOlympics2014WP/Controls/QuickSelector.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Input;
+
+namespace WinterOlympics2014WP.Controls
+{
+    public delegate void SelectionChangedEventHandler(object sender, int selectedIndex);
+
+    public partial class QuickSelector : UserControl
+    {
+        #region Property
+
+        private const double UNIT_HEIGHT = 26d;
+        private double offset_y = 0d;
+        private List<string> _keys;
+        private Dictionary<string, bool> _items;
+        private bool eventRaised = false;
+
+        private int _selectedIndex = -1;
+        public int SelectedIndex
+        {
+            get
+            {
+                return _selectedIndex;
+            }
+            set
+            {
+                if (_selectedIndex != value)
+                {
+                    _selectedIndex = value;
+                }
+            }
+        }
+
+        #endregion
+
+        public QuickSelector()
+        {
+            InitializeComponent();
+            selectionHint.Height = UNIT_HEIGHT;
+            VisualStateManager.GoToState(this, "SelectionPreviewHidden", false);
+        }
+
+        public void SetItems(Dictionary<string, bool> items)
+        {
+            this.itemsControl.ItemsSource = _items = items;
+            _keys = _items.Keys.ToList();
+            offset_y = UNIT_HEIGHT * ((items.Count - 1) * 0.5d);
+        }
+
+        private void SetSelectionPosition(MouseEventArgs e)
+        {
+            eventRaised = false;
+            VisualStateManager.GoToState(this, "SelectionPreviewShown", true);
+            Point p = e.GetPosition(LayoutRoot);
+
+            double y = p.Y - p.Y % UNIT_HEIGHT;
+            int tempIndex = (int)(y / UNIT_HEIGHT);
+
+            if (tempIndex < 0 || tempIndex >= _items.Count)
+            {
+                return;
+            }
+
+            SelectedIndex = tempIndex;
+
+            y = y - offset_y;
+
+            selectionHintTransform.TranslateY = y;
+            //selectionPreviewTransform.TranslateY = y;
+            selectionPreviewTransform.TranslateY = p.Y - offset_y - UNIT_HEIGHT * 0.5d;
+
+            selectionPreviewText.Text = _keys[SelectedIndex];
+        }
+
+        private void RaiseSelectionChanged()
+        {
+            if (eventRaised)
+            {
+                return;
+            }
+            eventRaised = true;
+            VisualStateManager.GoToState(this, "SelectionPreviewHidden", true);
+
+            if (_items[_keys[SelectedIndex]])
+            {
+                if (this.SelectionChanged != null)
+                {
+                    SelectionChanged(this, _selectedIndex);
+                }
+            }
+        }
+
+        #region Selection Changed Event
+
+        public event SelectionChangedEventHandler SelectionChanged;
+
+        #endregion
+
+        #region Touch Event
+
+        private void LayoutRoot_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            SetSelectionPosition(e);
+        }
+
+        private void LayoutRoot_MouseMove(object sender, MouseEventArgs e)
+        {
+            SetSelectionPosition(e);
+        }
+
+        private void LayoutRoot_MouseLeave(object sender, MouseEventArgs e)
+        {
+            RaiseSelectionChanged();
+        }
+
+        private void LayoutRoot_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            RaiseSelectionChanged();
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ WinterOlympics2014WP/WinterOlympics2014WP/Pages/HomePage.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace WinterOlympics2014WP.Pages
+{
+    public partial class HomePage : PhoneApplicationPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+            InitQuickSelector();
+        }
+
+        #region Pivot Selection
+
+        private void Pivot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (this.pivot.SelectedIndex)
+            {
+                case 0:
+                    SelectToday();
+                    break;
+                case 1:
+                    SelectTomorrow();
+                    break;
+                case 2:
+                    SelectYesterday();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void PivotHeader_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            string tag = (sender as FrameworkElement).Tag.ToString();
+            switch (tag)
+            {
+                case "yesterday":
+                    pivot.SelectedIndex = 2;
+                    break;
+                case "today":
+                    pivot.SelectedIndex = 0;
+                    break;
+                case "tomorrow":
+                    pivot.SelectedIndex = 1;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void SelectYesterday()
+        {
+            VisualStateManager.GoToState(this, "YesterdayActive", true);
+        }
+
+        private void SelectToday()
+        {
+            VisualStateManager.GoToState(this, "TodayActive", true);
+        }
+
+        private void SelectTomorrow()
+        {
+            VisualStateManager.GoToState(this, "TomorrowActive", true);
+        }
+
+        #endregion
+
+        #region Quick Selector
+
+        Dictionary<string, bool> hoursOfDay = new Dictionary<string, bool>();
+        List<string> hoursList = new List<string>();
+        private void InitQuickSelector()
+        {
+            hoursOfDay.Add("00:00", true);
+            hoursOfDay.Add("01:00", false);
+            hoursOfDay.Add("02:00", true);
+            hoursOfDay.Add("03:00", true);
+            hoursOfDay.Add("04:00", false);
+            hoursOfDay.Add("05:00", false);
+            hoursOfDay.Add("06:00", false);
+            hoursOfDay.Add("07:00", false);
+            hoursOfDay.Add("08:00", false);
+            hoursOfDay.Add("09:00", false);
+            hoursOfDay.Add("10:00", false);
+            hoursOfDay.Add("11:00", false);
+            hoursOfDay.Add("12:00", false);
+            hoursOfDay.Add("13:00", false);
+            hoursOfDay.Add("14:00", false);
+            hoursOfDay.Add("15:00", false);
+            hoursOfDay.Add("16:00", false);
+            hoursOfDay.Add("17:00", false);
+            hoursOfDay.Add("18:00", false);
+            hoursOfDay.Add("19:00", true);
+            hoursOfDay.Add("20:00", true);
+            hoursOfDay.Add("21:00", true);
+            hoursOfDay.Add("22:00", true);
+            hoursOfDay.Add("23:00", true);
+            quickSelector.SetItems(hoursOfDay);
+
+            hoursList = hoursOfDay.Keys.ToList();
+        }
+
+        private void QuickSelector_SelectionChanged(object sender, int selectedIndex)
+        {
+            MessageBox.Show(hoursList[selectedIndex]);
+        }
+
+        #endregion
+
+    }
+}
