52d957c0afeadd6f4efc01976a47124ccc2a2d66
elecpiano <elecpiano@gmail.com>
2014-01-03 23:39:04 +0800

    splash page

---
+++ WinterOlympics2014WP/WinterOlympics2014WP/Pages/SplashPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Media.Imaging;
+
+namespace WinterOlympics2014WP.Pages
+{
+    public partial class SplashPage : PhoneApplicationPage
+    {
+        public SplashPage()
+        {
+            InitializeComponent();
+            SetSplashImage();
+        }
+
+        #region Set Splash Image
+
+        private void SetSplashImage()
+        {
+            SetDefualtSplashImage();
+        }
+
+        private void SetDefualtSplashImage()
+        {
+            this.splashImage.Source = new BitmapImage(new Uri("/Assets/Images/SplashScreenDefault.PNG", UriKind.Relative));
+        }
+
+        #endregion
+    }
+}
